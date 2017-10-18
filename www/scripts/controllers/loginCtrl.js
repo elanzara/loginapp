02865e6e3915ff93e9d08691cb845d2fdc3d924e
elanzara <edulanzara@Gmail.com>
2017-10-18 17:37:44 -0300

    aaaa

--- www/scripts/controllers/loginCtrl.js
+++ www/scripts/controllers/loginCtrl.js
@@ -14,28 +14,27 @@ function LoginCtrl ($scope, $window, $cookies, $location, UserService, HOME) {
 		$scope.showLoading = true;
 		if(validateForm()){
 			
-			// UserService.setAuth($scope.user).success(function(response){
-			// 	$scope.showMessage = false;
-			// 	if(response.code == 200){
-			// 		var expireDate = new Date(Date.now()+1*24*60*60*1000);
-			// 		$cookies.putObject('token', response.user.token, {'expires': expireDate});
-			// 		$cookies.putObject('uid', response.user.id, {'expires': expireDate});
-			// 		$cookies.putObject('name', response.user.user, {'expires': expireDate});
-			// 		$cookies.putObject('idp', response.user.idprofiles, {'expires': expireDate});
-			// 		if(response.user.firstlogin != 0){
-			// 			$window.location.href = HOME;
-			// 		}else{
-			// 			$location.path('/cambiar-clave');
-			// 		}
+			UserService.setAuth($scope.user).success(function(response){
+				$scope.showMessage = false;
+				if(response.code == 200){
+					var expireDate = new Date(Date.now()+1*24*60*60*1000);
+					$cookies.putObject('token', response.user.token, {'expires': expireDate});
+					$cookies.putObject('uid', response.user.id, {'expires': expireDate});
+					$cookies.putObject('name', response.user.user, {'expires': expireDate});
+					$cookies.putObject('idp', response.user.idprofiles, {'expires': expireDate});
+					if(response.user.firstlogin != 0){
+						$window.location.href = HOME;
+					}else{
+						$location.path('/cambiar-clave');
+					}
 					
-			// 	}else{
-			// 		$scope.showMessage = true;
-			// 		$scope.showMessageTxt = 'Usuario o Clave Invalidos.';
-			// 	}
-			// 	$scope.showLoading = false;
-			// })
-			$window.location.href = HOME;
-			$scope.showLoading = false;
+				}else{
+					$scope.showMessage = true;
+					$scope.showMessageTxt = 'Usuario o Clave Invalidos.';
+				}
+				$scope.showLoading = false;
+			})
+
 		}else{
 			$scope.showMessage = true;
 			$scope.showMessageTxt = 'Usuario o Clave Incompletos.';
