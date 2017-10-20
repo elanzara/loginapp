e9d93184fcb0bd019881d972eec808e402f140b7
elanzara <edulanzara@Gmail.com>
2017-10-20 02:13:24 -0300

    compile 2

--- www/scripts/services/userService.js
+++ www/scripts/services/userService.js
@@ -1,7 +1,7 @@
 function UserService($http, $location, $cookies) {
 
 	var userResponse = {};
-	var apiUrl = 'http://192.168.0.35/mvss/mvssapi/v1/users/';/*'http://'+$location.host()+*/
+	var apiUrl = 'http://192.168.1.47/mvss/mvssapi/v1/users/';/*'http://'+$location.host()+*/
 	//   var apiUrl = 'http://m4000521.ferozo.com/mvssapi/v1/users/';/*'http://'+$location.host()+*/
 	var config = {
 		headers:  {'Authorization': $cookies.getObject('token'), 'SID': $cookies.getObject('uid')}
